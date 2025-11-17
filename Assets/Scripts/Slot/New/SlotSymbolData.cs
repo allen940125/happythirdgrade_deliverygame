9bcfe71221390345ba62510df4d6952256f66c41
zx <zzzzzzxxxxxx930922@gmail.com>
2025-11-17 13:03:45 +0800

    拉霸改版中

---
+++ Assets/Scripts/Slot/New/SlotSymbolData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "SlotSymbolData", menuName = "SlotMachine/SymbolData")]
+public class SlotSymbolData : ScriptableObject
+// 用於定義老虎機符號的資料(獎項符號圖形 權重)
+{
+    [System.Serializable]
+    public class SymbolInfo
+    {
+        public string symbolName; // 例如 "Diamond", "Crown"
+        public Sprite symbolSprite; // 要顯示的圖形
+        public int Weight; // 機率權重 
+    }
+}
